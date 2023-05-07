6bbc9b016ab6d211eb5166d31b431a54d2d408c7
Saurabh <saurabhm1309@gmail.com>
2023-05-07 11:20:10 +0530

    Created Splash Screen

---
+++ app/src/main/java/com/example/notes/ui/CreateNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.notes.R
+
+class CreateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
